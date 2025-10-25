ae1bebbbf2abd0bd4da8e491456a0f86432f8dd3
SvitlanaShustur <svsh.data.analytics@gmail.com>
2025-10-25 21:27:20 +0200

    завершила завдання з рядками

---
+++ for_while.py
@@ -0,0 +1,22 @@
+# for i in "Strit":
+#     print(i, end="")
+#     print(1, end="")
+
+# while True:
+#     print(1)
+# for i in range(5):
+#     print(i, end=", ")
+# n = 10
+# while True
+# while n > 0:
+#     print(1, end="")
+#     n -= 1
+n = 10
+while n > 0:
+    if n % 2 == 0:
+        n -= 1
+        continue 
+    elif n == 3:
+        break
+    print(n, end="    ")
+    n -= 3
